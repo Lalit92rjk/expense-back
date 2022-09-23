6276098b22c63dc924d699119b46e4a9d8725b44
Lalit Rajak <lalit92rajak@gmail.com>
2022-09-24 00:21:16 +0530

    login backend

--- controller/user.js
+++ controller/user.js
@@ -25,9 +25,31 @@ const signup  =  async (req, res)=>{
 }
 
 
+const login = (req,res)=>{
+    const {email,password} =  req.body
+    if(isstringinvalid(email)|| isstringinvalid(password)){
+        return res.status(400).json({message:"emailid or password is missing",success:false})
+    }
+    console.log(password);
+    User.findAll({where:{email}}).then(user=>{
+        if(user.length>0){
+            if(user[0].password===password){
+                res.status(200).json({success:true,message:"user logged in successfully"})
+            }else{
+                return res.status(400).json(({success:false,message:"password is incorrect"}))
+            }
+        }else{
+            return res.status(404).json({success:false,message:"user doesn't exist"})
+        }
+    }).catch(err=>{
+        res.status(500).json({message:err,success:false})
+    })
+}
+
+
 module.exports = {
         signup,
-        
+        login
     
     }
     
