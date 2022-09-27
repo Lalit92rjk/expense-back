e4b834d3961bb6e99bb6bc31a7029fe2dc53faff
Lalit Rajak <lalit92rajak@gmail.com>
2022-09-28 00:02:21 +0530

    premium

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ var cors = require('cors')
 const sequelize = require('./util/database');
 const User = require('./models/users');
 const Expense = require('./models/expenses');
+const Order = require('./models/orders');
 
 
 const userRoutes = require('./routes/user')
 const expenseRoutes =  require  ('./routes/expense')
+const purchaseRoutes = require('./routes/purchase')
 
 
 const app = express();
@@ -30,9 +32,13 @@ app.use(express.json());  //this is for handling jsons
 
 app.use('/user', userRoutes)
 app.use('/expense',expenseRoutes)
+app.use('/purchase', purchaseRoutes)
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
 
 
 sequelize
